a4d6b659bbae14be53c6e553ec6ed9c6a8649c58
Martin Ludueña <mlarcb@gmail.com>
2019-07-17 17:32:22 -0300

    primera parte

--- multilenguaje/05-f-from-c/c-main.c
+++ multilenguaje/05-f-from-c/c-main.c
@@ -4,15 +4,19 @@
 
 #define NUM 200
 
+
+extern void sum_abs_(int *inp, int *num, int *asum);
+
 int main(int argc, char **argv)
 {
     int data[NUM], num, i;
+    int asum;
 
     num = NUM;
     for (i=0; i < num; i++) {
         data[i] = i - 100 + 1;  /* NOTE: difference in array numbering */  
     }
-
-    printf("sum=%d\n", sum_abs(data, num));
+    sum_abs_(data, &num, &asum);
+    printf("sum=%d\n", asum);
     return 0;
 }
